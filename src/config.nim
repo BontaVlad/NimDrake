2ef45e6474d9149ab7d77d5dbddbc9dea6e26635
Sergiu Vlad Bonta <bonta.vlad@gmail.com>
2025-01-15 15:03:47 +0200

    fix code

--- src/config.nim
+++ src/config.nim
@@ -23,7 +23,7 @@ proc setConfig*(config: Config, name: string, option: string) =
   )
 
 proc newConfig*(): Config =
-  result = newConfig()
+  result = Config(nil)
   check(duckdbCreateConfig(result.addr), "Failed to create config")
 
 proc newConfig*(values: ConfigValues): Config =
