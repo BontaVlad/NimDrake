f84d07aa1a59cf3e8055bce03e13358b43f9f7e0
Sergiu Vlad Bonta <bonta.vlad@gmail.com>
2025-01-21 18:03:07 +0200

    Add missing benchmarks folder

---
+++ benchmarks/benchmark_scalar_functions.nim
@@ -0,0 +1,22 @@
+import criterion
+import ../src/nimdrake
+
+var cfg = newDefaultConfig()
+cfg.warmupBudget = 0.5
+cfg.budget = 0.1
+cfg.minSamples = 10
+cfg.verbose = true
+
+benchmark cfg:
+
+  proc benchmarkExecute() {.measure.} =
+    let conn = newDatabase().connect()
+
+    template doubleValue(val, bar: int64): int64 {.scalar.} =
+      return val * bar
+
+    conn.register(doubleValue)
+
+    conn.execute("CREATE TABLE test_table AS SELECT i FROM range(30000) t(i);")
+    let outcome =
+      conn.execute("SELECT i, doubleValue(i, i) as doubled FROM test_table").fetchAll()
